bdfc551e6d77cf889dc17f13e999f3dd3d76cf73
Cendres42 <chris@agilack.fr>
2020-04-15 12:44:48 +0200

    fonction ajout panier

--- main.js
+++ main.js
@@ -1,144 +1,25 @@
-//
+//fonction d'affichage des produits
+function affichage_produits(){
 var request=new XMLHttpRequest();
 request.onreadystatechange=function(){
-  if(this.readyState==XMLHttpRequest.DONE && this.status==200){
-    var response=JSON.parse(this.responseText);
-    for (let i = 0; i <5; i++){
-      let qs = "#Bear-"+i+ " h2 span.name";
-      document.querySelector(qs).innerHTML=response[i].name;
+  if(this.readyState==XMLHttpRequest.DONE && this.status==200){ //vérification serveur prêt à répondre
+    var response=JSON.parse(this.responseText); //décodage données JSON
+    for (let i = 0; i <5; i++){ //boucle sur le nombre de produits
+      let qs = "#Bear-"+i+ " h2 span.name"; //sélecteur
+      document.querySelector(qs).innerHTML=response[i].name; //récupération du nom
       qs = "#Bear-"+i+" .prix span";
-      document.querySelector(qs).innerHTML=response[i].price;
+      document.querySelector(qs).innerHTML=response[i].price;//récupération du prix
       qs="#Bear-"+i+ " p";
-      document.querySelector(qs).innerHTML=response[i].description;
+      document.querySelector(qs).innerHTML=response[i].description; //récupération de la description
       qs="#Bear-"+i+ " img";
       let img=document.querySelector(qs);
-      img.setAttribute("src",response[i].imageUrl);
+      img.setAttribute("src",response[i].imageUrl);//récupération de l'image
       qs="#Bear-"+i+ " a";
       let lien=document.querySelector(qs);
-      lien.setAttribute("href","teddy_perso.html?product_id="+response[i]._id);
+      lien.setAttribute("href","perso.html?product_id="+response[i]._id);//récupération de l'id pour afficher le bon produit via le lien
       }
-}
-};
-request.open("GET", "http://localhost:3000/api/teddies");
-request.send();
-
-
-
-
-
-
-
-//  let myHeading = document.querySelector('h1');
-  //myHeading.textContent = 'Bonjour, monde !';
-  //let titres2 = document.querySelector('h2');
-  //titres2.innerHTML = "<ul><li>Ours en peluche</li><li>Caméras</li></ul>";
-  /*function essaiPost(){
-  var send = new XMLHttpRequest();
-  send.onreadystatechange=function(){
-    if(this.readyState==XMLHttpRequest.DONE && this.status==200){
-    var response=JSON.parse(this.responseText);
-    document.getElementById('result').innerHTML=response.postData.text;
-    }
-  };
-  send.open("POST", "https://mockbin.com/request");
-  send.setRequestHeader("Content-Type", "application/json");
-  let jsonBody={};
-  jsonBody.value=document.getElementById('value').value;
-  send.send(JSON.stringify(jsonBody));
-  }
-  const form=document.getElementById('essai');
-  //let envoi=form.getAttribute('type');
-  form.addEventListener('click',essaiPost);
-
-
-  let myInput=document.getElementById('code');
-  let bouton=document.getElementById('submit-btn');
-  myInput.addEventListener('input', function(e) {
-    var value = e.target.value;
-    if (/^CODE-/.test(value)) {
-        document.getElementById("code-validation").innerHTML = "Code valide";
-        bouton.removeAttribute('disabled','disabled');
-        } else {
-        document.getElementById("code-validation").innerHTML = "Code invalide";
-        bouton.setAttribute('disabled', 'disabled');
-
-    }
-});
-
-  function askWeather(){
-  var request=new XMLHttpRequest();
-  request.onreadystatechange=function(){
-    if(this.readyState==XMLHttpRequest.DONE && this.status==200){
-      var response=JSON.parse(this.responseText);
-      document.getElementById('weather-result').innerHTML=response.current_condition.condition;
-    }
-  };
-  request.open("GET", "https://www.prevision-meteo.ch/services/json/paris");
-  request.send();
-  }
-  const weather=document.getElementById('ask-weather');
-  weather.addEventListener('click',askWeather);
-
-class Produits{
-  constructor(id, name, price, description, imageUrl){
-    this.id=id;
-    this.name=name;
-    this.price=price;
-    this.description=description;
-    this.imageUrl=imageUrl;
   }
 }
-const input=document.querySelector('input');
-const log=document.getElementById('res-name');
-input.addEventListener('input',updateV);
-function updateV(b){
-    log.textContent=b.target.value;
+request.open("GET", "http://localhost:3000/api/teddies"); //ouverture de la requête GET
+request.send(); //envoi de la requête
 }
-const genre=document.getElementById('gender');
-const gdr=document.getElementById('res-gender');
-genre.addEventListener('input',updateValue);
-function updateValue(e){
-gdr.textContent=e.target.value;
-}
-const souris=document.getElementById('result');
-souris.addEventListener('mousemove', function(sourimove) {
-document.getElementById('mouse-x').innerHTML = sourimove.offsetX; // Coordonnée X de la souris dans l'élément
-document.getElementById('mouse-y').innerHTML = sourimove.offsetY; // Coordonnée Y de la souris dans l'élément
-});
-const newElt=document.createElement("h2");
-let elt=document.getElementById("Teddy");
-elt.appendChild(newElt);
-newElt.innerHTML="<span>Mon nouvel Ours</span>";
-newElt.classList.add("important");
-newElt.style.color="red";
-
-let eltA=document.querySelector("#plop>a");
-eltA.addEventListener('click',function(event){
-eltA.innerHTML="it works!!!!";
-event.preventDefault();
-});
-
-let eltParent=document.getElementById("parent");
-let eltCP=document.getElementById("parentCount");
-eltCP.innerHTML=0;
-eltParent.addEventListener('click',function(clicP){
-eltCP.innerHTML++;
-});
-
-let eltEnfant=document.getElementById("enfant");
-let eltCE=document.getElementById("enfantCount");
-eltCE.innerHTML=0;
-eltEnfant.addEventListener('click',function(clicE){
-eltCE.innerHTML++;
-clicE.preventDefault();
-clicE.stopPropagation();
-});
-document.getElementById("enfantCount").innerHTMl=eltCE;
-
-$.getJSON("http://www.agilack.fr/data.json", function(data){
-  let produit=data;
-  console.log(produit);
-  $("h2 span").html("Ours en peluche "+produit.name);
-  $(".produit img").attr("src", produit.imageUrl);
-  $(".produit p").html(produit.description);
-});*/
--- perso.js
+++ perso.js
@@ -1,10 +1,20 @@
+//Déclaration des variables:
+let bouton_ajout_panier=document.getElementById("ajout-panier");
+let panier=[];
+let liste_produits="";
+let nouveau_produit="";
+let nouvelle_qte="";
 
+//Boutons d'action:
+bouton_ajout_panier.addEventListener("click",ajout_panier);
+
+//fonction d'affichage de l'ours sur lequel l'utilisaeur a cliqué
   var request=new XMLHttpRequest();
   request.onreadystatechange=function (){
-    if(this.readyState==XMLHttpRequest.DONE && this.status==200){
+    if(this.readyState==XMLHttpRequest.DONE && this.status==200){ //vérification serveur prêt à répondre
       var response=JSON.parse(this.responseText);
-      let qs = "#Bear h2 span.name";
-      document.querySelector(qs).innerHTML=response.name;
+      let qs = "#Bear h2 span.name"; //idem fonction affichage produits
+      document.querySelector(qs).innerHTML=response.name; //récupération des éléments le concernant
       qs = "#Bear .prix span";
       document.querySelector(qs).innerHTML=response.price;
       qs="#Bear p";
@@ -20,7 +30,61 @@
     }
   }
   };
-  let adresse=window.location.search;
-  let identifiant=(adresse.replace('?product_id=', ''));
-request.open("GET", "http://localhost:3000/api/teddies/"+identifiant);
-request.send();
+  let adresse=window.location.search; //récupération dans la barre d'adresse de l'id-produit
+  let identifiant=(adresse.replace('?product_id=', ''));  //récupération dans la barre d'adresse de l'id-produit
+  request.open("GET", "http://localhost:3000/api/teddies/"+identifiant); //ouverture de la requête GET pour l'id récupéré
+  request.send();//envoi de la requête
+
+//function ajout_panier(){
+
+function ajout_panier(){
+  panier= JSON.parse(sessionStorage.getItem("liste_produits_panier"));
+  if(panier==null){
+  panier=[];
+  }
+  let new_product={};
+  new_product.id=identifiant;
+  new_product.name=document.querySelector("#Bear h2 span.name").innerHTML;
+  new_product.qte=document.getElementById("qt").value;
+  let found=0;
+  for (let i=0; i<panier.length;i++){
+    if(new_product.id==panier[i].id){
+      panier[i].qte=parseInt(panier[i].qte, 10);
+      new_product.qte=parseInt(new_product.qte, 10);
+      panier[i].qte= panier[i].qte+new_product.qte;
+      nouvelle_qte.innerHTML=panier[i].qte;
+      found=1;
+    }
+  }
+    if(found==0){
+    panier.push(new_product);
+    alert(sessionStorage.getItem("liste_produits_panier"));
+    liste_produits=document.getElementById("panier");
+    nouveau_produit = document.createElement('produit');
+    liste_produits.appendChild(nouveau_produit);
+    nouveau_produit.innerHTML= new_product.name;
+    nouvelle_qte = document.createElement('qte');
+    liste_produits.appendChild(nouvelle_qte);
+    nouvelle_qte.innerHTML= new_product.qte;
+  }
+sessionStorage.setItem("liste_produits_panier", JSON.stringify(panier));
+}
+
+
+//fonction création panier
+function recup_panier(){
+  panier= JSON.parse(sessionStorage.getItem("liste_produits_panier"));
+  if(panier==null){
+  panier=[];
+  }
+  for (let i=0; i<panier.length; i++){
+    let new_product=panier[i];
+    liste_produits=document.getElementById("panier");
+    nouveau_produit = document.createElement('produit');
+    liste_produits.appendChild(nouveau_produit);
+    nouveau_produit.innerHTML=new_product.name;
+    nouvelle_qte = document.createElement('qte');
+    liste_produits.appendChild(nouvelle_qte);
+    nouvelle_qte.innerHTML=new_product.qte;
+  }
+}
