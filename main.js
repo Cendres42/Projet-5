83ac6193a3d0d8cc443ca0c3811aa275f1ad2989
Cendres42 <chris@agilack.fr>
2020-04-17 21:28:03 +0200

    Version bien indentée

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 //fonction d'affichage des produits
+
+let response=[];
+
 function affichage_produits(){
 var request=new XMLHttpRequest();
 request.onreadystatechange=function(){
   if(this.readyState==XMLHttpRequest.DONE && this.status==200){ //vérification serveur prêt à répondre
-    var response=JSON.parse(this.responseText); //décodage données JSON
+    response=JSON.parse(this.responseText);
+    //décodage données JSON
     for (let i = 0; i <5; i++){ //boucle sur le nombre de produits
       let qs = "#Bear-"+i+ " h2 span.name"; //sélecteur
       document.querySelector(qs).innerHTML=response[i].name; //récupération du nom
@@ -18,8 +22,32 @@ request.onreadystatechange=function(){
       let lien=document.querySelector(qs);
       lien.setAttribute("href","perso.html?product_id="+response[i]._id);//récupération de l'id pour afficher le bon produit via le lien
       }
+    }
   }
-}
 request.open("GET", "http://localhost:3000/api/teddies"); //ouverture de la requête GET
 request.send(); //envoi de la requête
+}
+
+//fonction création panier
+function recup_panier(){
+  panier= JSON.parse(sessionStorage.getItem("liste_produits_panier"));
+  identifiant=(sessionStorage.getItem("identifiant"));
+  if(panier==null){
+  panier=[];
+  }
+  for (let i=0; i<panier.length; i++){
+    let new_product=panier[i];
+    liste_produits=document.getElementById("panier");
+    nouvelle_colonne = document.createElement('tr');
+    nouvelle_colonne.setAttribute("id","panier-"+identifiant);
+    liste_produits.appendChild(nouvelle_colonne);
+    nouveau_produit = document.createElement('td');
+    nouveau_produit.setAttribute("class","pdt");
+    nouvelle_colonne.appendChild(nouveau_produit);
+    nouveau_produit.innerHTML=new_product.name;
+    nouvelle_qte= document.createElement('td');
+    nouvelle_qte.setAttribute("class","qte");
+    nouvelle_colonne.appendChild(nouvelle_qte);
+    nouvelle_qte.innerHTML= new_product.qte;
+    }
 }
--- perso.js
+++ perso.js
@@ -5,6 +5,7 @@ let liste_produits="";
 let nouveau_produit="";
 let nouvelle_qte="";
 
+
 //Boutons d'action:
 bouton_ajout_panier.addEventListener("click",ajout_panier);
 
@@ -23,17 +24,19 @@ bouton_ajout_panier.addEventListener("click",ajout_panier);
       let img=document.querySelector(qs);
       img.setAttribute("src",response.imageUrl);
       for(let u=0;u<response.colors.length;u++){
-      qs="#Bear select";
-      let option=document.createElement("option");
-      option.innerHTML=response.colors[u];
-      document.querySelector(qs).appendChild(option);
+        qs="#Bear select";
+        let option=document.createElement("option");
+        option.innerHTML=response.colors[u];
+        document.querySelector(qs).appendChild(option);
+      }
     }
   }
-  };
   let adresse=window.location.search; //récupération dans la barre d'adresse de l'id-produit
   let identifiant=(adresse.replace('?product_id=', ''));  //récupération dans la barre d'adresse de l'id-produit
   request.open("GET", "http://localhost:3000/api/teddies/"+identifiant); //ouverture de la requête GET pour l'id récupéré
   request.send();//envoi de la requête
+  sessionStorage.setItem("identifiant", identifiant);
+
 
 //function ajout_panier(){
 
@@ -46,45 +49,57 @@ function ajout_panier(){
   new_product.id=identifiant;
   new_product.name=document.querySelector("#Bear h2 span.name").innerHTML;
   new_product.qte=document.getElementById("qt").value;
+  new_product.price=document.querySelector("#Bear .prix span").innerHTML;
   let found=0;
   for (let i=0; i<panier.length;i++){
     if(new_product.id==panier[i].id){
       panier[i].qte=parseInt(panier[i].qte, 10);
       new_product.qte=parseInt(new_product.qte, 10);
       panier[i].qte= panier[i].qte+new_product.qte;
+      nouvelle_qte=document.querySelector("#panier-"+identifiant+" .qte");
       nouvelle_qte.innerHTML=panier[i].qte;
       found=1;
     }
   }
     if(found==0){
     panier.push(new_product);
-    alert(sessionStorage.getItem("liste_produits_panier"));
     liste_produits=document.getElementById("panier");
-    nouveau_produit = document.createElement('produit');
-    liste_produits.appendChild(nouveau_produit);
-    nouveau_produit.innerHTML= new_product.name;
-    nouvelle_qte = document.createElement('qte');
-    liste_produits.appendChild(nouvelle_qte);
+    nouvelle_colonne = document.createElement('tr');
+    nouvelle_colonne.setAttribute("id","panier-"+identifiant);
+    liste_produits.appendChild(nouvelle_colonne);
+    nouveau_produit = document.createElement('td');
+    nouveau_produit.setAttribute("class","pdt");
+    nouvelle_colonne.appendChild(nouveau_produit);
+    nouveau_produit.innerHTML=new_product.name;
+    nouvelle_qte= document.createElement('td');
+    nouvelle_qte.setAttribute("class","qte");
+    nouvelle_colonne.appendChild(nouvelle_qte);
     nouvelle_qte.innerHTML= new_product.qte;
-  }
+    }
 sessionStorage.setItem("liste_produits_panier", JSON.stringify(panier));
 }
 
 
-//fonction création panier
+//fonction récupération panier
 function recup_panier(){
   panier= JSON.parse(sessionStorage.getItem("liste_produits_panier"));
+  identifiant=(sessionStorage.getItem("identifiant"));
   if(panier==null){
   panier=[];
   }
   for (let i=0; i<panier.length; i++){
     let new_product=panier[i];
     liste_produits=document.getElementById("panier");
-    nouveau_produit = document.createElement('produit');
-    liste_produits.appendChild(nouveau_produit);
+    nouvelle_colonne = document.createElement('tr');
+    nouvelle_colonne.setAttribute("id","panier-"+identifiant);
+    liste_produits.appendChild(nouvelle_colonne);
+    nouveau_produit = document.createElement('td');
+    nouveau_produit.setAttribute("class","pdt");
+    nouvelle_colonne.appendChild(nouveau_produit);
     nouveau_produit.innerHTML=new_product.name;
-    nouvelle_qte = document.createElement('qte');
-    liste_produits.appendChild(nouvelle_qte);
-    nouvelle_qte.innerHTML=new_product.qte;
+    nouvelle_qte= document.createElement('td');
+    nouvelle_qte.setAttribute("class","qte");
+    nouvelle_colonne.appendChild(nouvelle_qte);
+    nouvelle_qte.innerHTML= new_product.qte;
   }
 }
